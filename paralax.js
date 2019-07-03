14acfef2e9f4474895288b3353907e588acbe53d
manoj <manoj kumar>
2019-07-03 11:35:53 +0530

    n new

--- paralax.js
+++ paralax.js
@@ -1,11 +1,123 @@
-var image = document.getElementsByClassName('u-image u-image-default u-opacity u-opacity-10 u-image-1');
-new simpleParallax(image, {
-    overflow: true,
-    orientation: 'right'
-});
-var image = document.getElementsByClassName('u-image u-image-default u-image-2');
+var image = document.getElementsByClassName('rimg u-image u-image-default u-opacity u-opacity-10 u-image-1');
 new simpleParallax(image, {
     overflow: true,
     orientation: 'left',
-    scale:1.5
+    scale:1.2,
+    delay:5
 });
+var image = document.getElementsByClassName('img u-image u-image-default u-image-2');
+new simpleParallax(image, {
+    overflow: true,
+    orientation: 'right',
+    scale:1.3,
+    delay:5
+});
+var image = document.getElementsByClassName('u-image u-image-default wireless_bt u-image-3');
+new simpleParallax(image, {
+    overflow: true,
+    orientation: 'down-right',
+    scale:3.9,
+    delay:5
+});
+
+
+// the end of the first section of the parallax effect//
+
+var image = document.getElementsByClassName('eimg u-image u-image-default u-opacity u-opacity-10 u-image-1');
+new simpleParallax(image, {
+    overflow: true,
+    orientation: 'left',
+    scale:1.2,
+    delay:5
+});
+var image = document.getElementsByClassName('u-image u-image-default voice_assistant u-image-2');
+new simpleParallax(image, {
+    overflow: true,
+    orientation: 'right',
+    scale:1.3,
+    delay:5
+});
+var image = document.getElementsByClassName('img2 u-image u-image-default u-image-3');
+new simpleParallax(image, {
+    overflow: true,
+    orientation: 'up-right',
+    scale:2.6,
+    delay:5
+});
+
+
+// the end of the second section of the parallax effect//
+
+var image = document.getElementsByClassName('gimg u-image u-image-default u-opacity u-opacity-10 u-image-1');
+new simpleParallax(image, {
+    overflow: true,
+    orientation: 'left',
+    scale:1.2,
+    delay:5
+});
+var image = document.getElementsByClassName('ergnomic_design u-image u-image-default u-image-3');
+new simpleParallax(image, {
+    overflow: true,
+    orientation: 'right',
+    scale:1.3,
+    delay:5
+});
+var image = document.getElementsByClassName('img3 u-image u-image-default u-image-2');
+new simpleParallax(image, {
+    overflow: true,
+    orientation: 'up-right',
+    scale:2.6,
+    delay:5
+});
+
+
+// the end of the third section of the parallax effect//
+
+var image = document.getElementsByClassName('aimg u-image u-image-default u-opacity u-opacity-10 u-image-1');
+new simpleParallax(image, {
+    overflow: true,
+    orientation: 'left',
+    scale:1.2,
+    delay:5
+});
+var image = document.getElementsByClassName('foldable_headband u-image u-image-default u-image-3');
+new simpleParallax(image, {
+    overflow: true,
+    orientation: 'right',
+    scale:1.3,
+    delay:5
+});
+var image = document.getElementsByClassName('img4 u-image u-image-default u-image-2');
+new simpleParallax(image, {
+    overflow: true,
+    orientation: 'up-right',
+    scale:2.6,
+    delay:5
+});
+
+
+// the end of the fourth section of the parallax effect//
+var image = document.getElementsByClassName('limg u-image u-image-default u-opacity u-opacity-10 u-image-2');
+new simpleParallax(image, {
+    overflow: true,
+    orientation: 'left',
+    scale:1.2,
+    delay:5
+});
+var image = document.getElementsByClassName('_hrs u-image u-image-default u-image-3');
+new simpleParallax(image, {
+    overflow: true,
+    orientation: 'right',
+    scale:1.3,
+    delay:5
+});
+var image = document.getElementsByClassName('img5 u-image u-image-default u-image-1');
+new simpleParallax(image, {
+    overflow: true,
+    orientation: 'down-left',
+    scale:2.6,
+    delay:5
+});
+
+
+// the end of the fifth section of the parallax effect//
